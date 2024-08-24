e8f2c47517ae7194d6da6f9c4ca9144cba07fbf5
Thomas Walsh <Thomasmwalsh217@gmail.com>
2024-08-24 03:34:46 -0400

    Finished the final project

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -106,7 +106,7 @@ class Question(models.Model):
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.CharField(max_length=200)
-    correct = models.BooleanField(default=False)
+    is_correct = models.BooleanField(default=False)
 
     def __str__(self):
         return "Choice: " + self.content
